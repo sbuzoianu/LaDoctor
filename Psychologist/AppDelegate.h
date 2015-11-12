f4eb7a54e87816a219cf2ac878386372a7e70c4a
sbuzoianu <stefan@buzoianu.ro>
2015-11-12 09:42:34 +0200

    Initial Commit

---
+++ Psychologist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Psychologist
+//
+//  Created by Stefan on 12/11/15.
+//  Copyright © 2015 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Psychologist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Psychologist
+//
+//  Created by Stefan on 12/11/15.
+//  Copyright © 2015 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
